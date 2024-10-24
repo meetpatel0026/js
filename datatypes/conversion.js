15f50b8e7a7a7e9bc9532670080da35b56bb1404
meetpatel0026 <meetpatel0026@gmail.com>
2024-10-24 17:19:25 +0000

    type conversion

---
+++ datatypes/conversion.js
@@ -0,0 +1,31 @@
+let score = '33';
+
+console.log(typeof score);
+console.log(Number(score))
+
+let some = '123abd'
+console.log(typeof(some));
+console.log(Number(some));
+
+
+let stri = 26;
+console.log(typeof stri);
+console.log(String(stri))
+
+let bool = "true";
+console.log(typeof bool);
+console.log(Boolean(bool));
+
+let booll = "false";
+console.log(typeof booll);
+console.log(Boolean(booll));
+
+let digit = 1;
+let digit1 = 0;
+let r1 = '1dsasdasd';
+
+console.log(Boolean(digit));
+console.log(Boolean(digit1));
+console.log(Boolean(r1));
+
+
